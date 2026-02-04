f2563cf1dd07b45a8ce02854928f199a3b779bc7
Juan Giler <juangiler@icloud.com>
2026-02-05 00:07:21 +0100

    postgresql adapter added

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Caculator module
+"""
+
+def add(x,y):
+    """Return the sum of x and y."""
+    return x + y
+
+def substract(x,y):
+    """Substract y from x and return the result."""
+    return y - x
